66ddf1c9641797ef4551692b9e0538f69b0c813d
seba <sebastian.tapia.st@gmail.com>
2021-04-02 18:03:32 -0300

    Agrega ejemplos

---
+++ Ejercicios/Ejercicio_3/ex_4shapes.py
@@ -0,0 +1,169 @@
+# coding=utf-8
+"""Drawing 4 shapes with different transformations"""
+
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import basic_shapes as bs
+import easy_shaders as es
+import transformations as tr
+
+__author__ = "Daniel Calderon"
+__license__ = "MIT"
+
+
+# We will use 32 bits data, so an integer has 4 bytes
+# 1 byte = 8 bits
+SIZE_IN_BYTES = 4
+
+
+# A class to store the application control
+class Controller:
+    fillPolygon = True
+
+
+# we will use the global controller as communication with the callback function
+controller = Controller()
+
+
+def on_key(window, key, scancode, action, mods):
+
+    if action != glfw.PRESS:
+        return
+    
+    global controller
+
+    if key == glfw.KEY_SPACE:
+        controller.fillPolygon = not controller.fillPolygon
+
+    elif key == glfw.KEY_ESCAPE:
+        glfw.set_window_should_close(window, True)
+
+    else:
+        print('Unknown key')
+
+
+if __name__ == "__main__":
+
+    # Initialize glfw
+    if not glfw.init():
+        glfw.set_window_should_close(window, True)
+
+    width = 600
+    height = 600
+
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 1)
+    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+    window = glfw.create_window(width, height, "Displaying multiple shapes - Modern OpenGL", None, None)
+
+    if not window:
+        glfw.terminate()
+        glfw.set_window_should_close(window, True)
+
+    glfw.make_context_current(window)
+
+    # Connecting the callback function 'on_key' to handle keyboard events
+    glfw.set_key_callback(window, on_key)
+
+    # Binding artificial vertex array object for validation
+    VAO = glGenVertexArrays(1)
+    glBindVertexArray(VAO)
+
+    # Creating our shader program and telling OpenGL to use it
+    pipeline = es.SimpleTransformShaderProgram()
+    glUseProgram(pipeline.shaderProgram)
+
+    # Setting up the clear screen color
+    glClearColor(0.15, 0.15, 0.15, 1.0)
+
+    # Creating shapes on GPU memory
+    shapeTriangle = bs.createRainbowTriangle()
+    gpuTriangle = es.GPUShape().initBuffers()
+    gpuTriangle.fillBuffers(shapeTriangle.vertices, shapeTriangle.indices, GL_STATIC_DRAW)
+
+    shapeQuad = bs.createRainbowQuad()
+    gpuQuad = es.GPUShape().initBuffers()
+    gpuQuad.fillBuffers(shapeQuad.vertices, shapeQuad.indices, GL_STATIC_DRAW)
+
+    glBindVertexArray(gpuQuad.vao)
+
+    # Creating our shader program and telling OpenGL to use it
+    pipeline = es.SimpleTransformShaderProgram()
+    glUseProgram(pipeline.shaderProgram)
+    pipeline.setupVAO(gpuTriangle)
+    pipeline.setupVAO(gpuQuad)
+
+
+    while not glfw.window_should_close(window):
+        # Using GLFW to check for input events
+        glfw.poll_events()
+
+        # Filling or not the shapes depending on the controller state
+        if (controller.fillPolygon):
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        else:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+        # Clearing the screen in both, color and depth
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        # Using the time as the theta parameter
+        theta = glfw.get_time()
+
+        # Triangle
+        triangleTransform = tr.matmul([
+            tr.translate(0.5, 0.5, 0),
+            tr.rotationZ(2 * theta),
+            tr.uniformScale(0.5)
+        ])
+
+        # updating the transform attribute
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, triangleTransform)
+
+        # drawing function
+        pipeline.drawCall(gpuTriangle)
+
+        # Another instance of the triangle
+        triangleTransform2 = tr.matmul([
+            tr.translate(-0.5, 0.5, 0),
+            tr.scale(
+                0.5 + 0.2 * np.cos(1.5 * theta),
+                0.5 + 0.2 * np.sin(2 * theta),
+                0)
+        ])
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, triangleTransform2)
+        pipeline.drawCall(gpuTriangle)
+
+        # Quad
+        quadTransform = tr.matmul([
+            tr.translate(-0.5, -0.5, 0),
+            tr.rotationZ(-theta),
+            tr.uniformScale(0.7)
+        ])
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, quadTransform)
+        pipeline.drawCall(gpuQuad)
+
+        # Another instance of the Quad
+        quadTransform2 = tr.matmul([
+            tr.translate(0.5, -0.5, 0),
+            tr.shearing(0.3 * np.cos(theta), 0, 0, 0, 0, 0),
+            tr.uniformScale(0.7)
+        ])
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, quadTransform2)
+        pipeline.drawCall(gpuQuad)
+
+        # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
+        glfw.swap_buffers(window)
+
+    # freeing GPU memory
+    gpuTriangle.clear()
+    gpuQuad.clear()
+    
+    glfw.terminate()
