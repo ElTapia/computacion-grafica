23c0b0114e5b48fe48ada4a6a5035e10b25892a5
seba <sebastian.tapia.st@gmail.com>
2021-04-27 17:36:51 -0400

    Agrega tarea 1

---
+++ Tareas/Tarea_1/survival.py
@@ -0,0 +1,13 @@
+import sys
+
+# Inicializa parámetros
+Z = sys.argv[1]
+H = sys.argv[2]
+T = sys.argv[3]
+P = sys.argv[4]
+
+print("Inicia el juego")
+print("{} zombies".format(Z))
+print("{} humanos".format(H))
+print("{} segundos".format(T))
+print("{} probabilidad de contagio".format(P))
